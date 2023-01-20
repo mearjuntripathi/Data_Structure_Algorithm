e03fa5dbfed29920722d789fef1b1909b30be600
mearjuntripathi <rajr60666@gmail.com>
2023-01-20 17:43:14 +0530

    datastructure

--- Data Structure/Array/MoveAllZero.cpp
+++ Data Structure/Array/MoveAllZero.cpp
@@ -7,8 +7,10 @@ int main (void) {
     int n = sizeof(arr) / sizeof (arr[0]);
     int j=0;
     for(int i=0;i<n;i++)
-        if(arr[i])
-            swap(arr[j++],arr[i]);
+        if(arr[i]!=0)
+            arr[j++] = arr[i];
+    while(j<n)
+        arr[j++]=0;
     for(int i=0;i<n;i++)
         cout<<arr[i]<<", ";
     cout<<endl;
