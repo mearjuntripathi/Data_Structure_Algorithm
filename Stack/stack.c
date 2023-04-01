962c3376fd4a2d589004680e6c143aaff5a0383c
mearjuntripathi <rajr60666@gmail.com>
2023-04-01 15:53:32 +0530

    stack

--- Stack/stack.c
+++ Stack/stack.c
@@ -2,8 +2,39 @@
 #include <stdlib.h>
 #include <limits.h>
 
-struct Node{
-    int data;
-    Node *next;
-};
+#define MAX 100
 
+int stack[MAX];
+int tos = -1;
+
+void push(int data){
+    if(tos == MAX-1){
+        printf("\nStack is overflow condition");
+        return;
+    }
+    stack[++tos] = data;
+}
+
+int pop(){
+    if(tos == -1){
+        printf("\nStack is underflow condition");
+        return -1;
+    }
+    int data = stack[tos];
+    tos--;
+    return data;
+}
+
+int main(){
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+
+    printf("\n%d",pop());
+    printf("\n%d",pop());
+    printf("\n%d",pop());
+    printf("\n%d",pop());
+    printf("\n%d",pop());
+}
