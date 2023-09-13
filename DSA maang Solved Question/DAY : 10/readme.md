5f3350222af831cdbed7dd421fe10a8e86141d7c
mearjuntripathi <mearjuntripathi@gmail.com>
2023-09-13 15:21:28 +0530

    Day 10 is completed

---
+++ DSA maang Solved Question/DAY : 10/readme.md
@@ -0,0 +1,15 @@
+# Day : 10
+
+![day 10](../images/day10.png)
+
+## [1. Valid Palindrome](125.%20Valid%20Palindrome.md)
+
+## [2. Valid Palindrome II](680.%20Valid%20Palindrome%20II.md)
+
+## [3. Longest Palindrome](409.%20Longest%20Palindrome.md)
+
+## [4. Longest Common Prefix](14.%20Longest%20Common%20Prefix.md)
+
+## [5. Valid Anagram](14.%20Longest%20Common%20Prefix.md)
+
+## [6. First Unique Character in a String](387.%20First%20Unique%20Character%20in%20a%20String.md)
--- Tree/Binary Tree/Binary_tree.cpp
+++ Tree/Binary Tree/Binary_tree.cpp
@@ -12,9 +12,22 @@ class Binary_tree {
 
         Node(int data){
             this->data = data;
-            left_child = right_child = NULL;
+            this->left_child = NULL;
+            this->right_child = NULL;
         }
     };
+
+    Node* include_array(vector<int>&arr){
+        static int i = -1;
+        if(arr.size() == 0)return NULL;
+        if(arr[++i] == -1)
+            return NULL;
+        Node *temp = new Node(arr[i]);
+        temp->left_child = include_array(arr);
+        temp->right_child = include_array(arr);
+        return temp;
+    }
+
     public :
         Node *root;
         Binary_tree(){
@@ -23,36 +36,90 @@ class Binary_tree {
         Binary_tree(int data){
             root = new Node(data);
         }
+        Binary_tree(vector<int>&arr){
+            root = include_array(arr);
+        }
 
-        void depth_first_value(int arr[]){
-            if(arr.length() == 0)
-                return ;
-
+        void insert_array(vector<int> &arr) {
             int i = 0;
-            stack<Node *> st;
+            if (arr.empty())
+                return;
+
+            stack<Node*> st;
+            root = new Node(arr[i++]);
+            bool left = true;
+            bool right = false;
             st.push(root);
 
-            while(!st.empty()){
-                Node * curr = st.top();
-                st.pop();
-                if(arr[i] != -1){
-                    curr = NULL;
+            while (!st.empty() && i < arr.size()) {
+                // cout<<arr[i] << " l - " << left << " r - " <<right<<endl;
+                Node *temp = st.top();
+                if(left){
+                    if(arr[i] != -1){
+                        
+                        temp->left_child = new Node(arr[i]);
+                        st.push(temp->left_child);
+                    }else{
+                        // cout<<temp->data<< " l";
+                        left = false;
+                        right = true;
+                    }
+                }else{
+                    if(arr[i] != -1){
+                        
+                        temp->right_child = new Node(arr[i]);
+                        st.push(temp->right_child);
+                        left = true;
+                        right = false;
+                    }else{
+                        // cout<<temp->data<< " r";
+                        st.pop();
+                        while(!st.empty() && (st.top()->right_child != NULL))
+                            st.pop();
+                    }
                 }
-                else{
-                    curr = new Node[i++];
-                }
+                
+                i++;
+            }
+        }
 
+        void preorder_traversal(Node *root){
+            if(root == NULL){
+                cout<< -1 << ' ';
+                return ;
+            }
+            cout << root->data << ' ';
+            preorder_traversal(root->left_child);
+            preorder_traversal(root->right_child);
+        }
+
+        void preorder_traversal(){
+            if(root == NULL)
+                return;
+
+            stack<Node*> st;
+            st.push(root);
+            while(!st.empty()){
+                Node *curr = st.top();
+                st.pop();
+                cout << curr->data << ' ';
+                if(curr->right_child)
+                    st.push(curr->right_child);
+                if(curr->left_child)
+                    st.push(curr->left_child);
             }
         }
         
 };
 
 int main() {
-    Binary_tree a(1);
-    Binary_tree b(2);
-    Binary_tree c(3);
-    
-    a.root->right_child = b.root;
-    a.root->left_child = c.root;
+
+    vector<int> arr = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Binary_tree b;
+    b.insert_array(arr);
+    b.preorder_traversal();
+    cout << endl;
+    b.preorder_traversal(b.root);
+
     return 0;
 }
